538b5e5c9b7fa6c271ba72fa934d3485a51a3537
Oleh Volynets <saggot91@gmail.com>
2025-06-18 10:01:38 +0200

    export more util funcs

--- lua/sigma/util.lua
+++ lua/sigma/util.lua
@@ -9,13 +9,13 @@ local fgColor = "#F6FADE"
 local util = {}
 
 ---@param hex_str string hex color string
-local function hexToRgb(hex_str)
-  hex_str = string.lower(hex_str)
+function util.hexToRgb(hex_str)
+    hex_str = string.lower(hex_str)
 
-  assert(string.find(hex_str, pat) ~= nil, "hex_to_rgb: invalid hex_str: " .. tostring(hex_str))
+    assert(string.find(hex_str, pat) ~= nil, "hex_to_rgb: invalid hex_str: " .. tostring(hex_str))
 
-  local r, g, b = string.match(hex_str, pat)
-  return { tonumber(r, 16), tonumber(g, 16), tonumber(b, 16) }
+    local r, g, b = string.match(hex_str, pat)
+    return { tonumber(r, 16), tonumber(g, 16), tonumber(b, 16) }
 end
 
 ---@param a number color channel value
@@ -29,8 +29,8 @@ end
 ---@param b string background color
 ---@param alpha number number between 0 and 1. 0 results in bg, 1 results in fg
 function util.blend(a, b, alpha)
-    a = hexToRgb(a)
-    b = hexToRgb(b)
+    a = util.hexToRgb(a)
+    b = util.hexToRgb(b)
 
     return string.format("#%02X%02X%02X",
         blendChannel(a[1], b[1], alpha),
@@ -38,26 +38,26 @@ function util.blend(a, b, alpha)
         blendChannel(a[3], b[3], alpha))
 end
 
-
 ---@param hexCol string
 ---@param amount number
 function util.darken(hexCol, amount)
-  return util.blend(hexCol, bgColor, math.abs(amount))
+    return util.blend(hexCol, bgColor, math.abs(amount))
 end
+
 ---@param hexCol string
 ---@param amount number
 function util.lighten(hexCol, amount)
-  return util.blend(hexCol, fgColor, math.abs(amount))
+    return util.blend(hexCol, fgColor, math.abs(amount))
 end
 
 function util.brighten(color, percentage)
-  local hsl = hsluv.hex_to_hsluv(color)
-  local larpSpace = 100 - hsl[3]
-  if percentage < 0 then
-    larpSpace = hsl[3]
-  end
-  hsl[3] = hsl[3] + larpSpace * percentage
-  return hsluv.hsluv_to_hex(hsl)
+    local hsl = hsluv.hex_to_hsluv(color)
+    local larpSpace = 100 - hsl[3]
+    if percentage < 0 then
+        larpSpace = hsl[3]
+    end
+    hsl[3] = hsl[3] + larpSpace * percentage
+    return hsluv.hsluv_to_hex(hsl)
 end
 
 return util
