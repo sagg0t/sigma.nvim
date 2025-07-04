b7bd171eda37129c10e38c4a2470654cb909a365
Oleh Volynets <saggot91@gmail.com>
2025-07-04 23:04:47 +0200

    replace fzf with snacks

--- lua/sigma/theme/fzf.lua
+++
@@ -1,7 +0,0 @@
-local c = require("sigma.colors")
-
-return {
-    FzfLuaPreviewNormal = { bg = c.deadCyan },
-    FzfLuaPreviewBorder = { fg = c.fg, bg = c.deadCyan },
-    FzfLuaPreviewTitle  = { bg = c.deadCyan },
-}
---
+++ lua/sigma/theme/snacks.lua
@@ -0,0 +1,8 @@
+local c = require("sigma.colors")
+
+return {
+    SnacksPickerPreview = { bg = c.deadCyan },
+    SnacksPickerPreviewTitle = { bg = c.deadCyan },
+
+    SnacksPickerMatch = { fg = c.pinkNeon },
+}
